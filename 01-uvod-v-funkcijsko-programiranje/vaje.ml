a95219d67af484bcd1d71bc45db91bdeb43719fd
Filip Bratovič <filip.bratovic1@gmail.com>
2025-10-07 12:57:50 +0100

    1. vaje

--- 01-uvod-v-funkcijsko-programiranje/vaje.ml
+++ 01-uvod-v-funkcijsko-programiranje/vaje.ml
@@ -11,7 +11,12 @@
  predstavljen s seznamom števil s plavajočo vejico, pomnoži z danim skalarjem.
 [*----------------------------------------------------------------------------*)
 
-let razteg _ _ = ()
+let razteg koeficient vektor =
+  List.map (fun x -> x *. koeficient) vektor
+
+let razteg koeficient vektor =
+  let f_razteg x = koeficient *. x in
+  List.map f_razteg vektor
 
 let primer_vektorji_1 = razteg 2.0 [1.0; 2.0; 3.0]
 (* val primer_vektorji_1 : float list = [2.; 4.; 6.] *)
@@ -21,7 +26,12 @@ let primer_vektorji_1 = razteg 2.0 [1.0; 2.0; 3.0]
  vsoto dveh vektorjev.
 [*----------------------------------------------------------------------------*)
 
-let sestej _ _ = ()
+let sestej vek1 vek2 =
+  List.map2 (+.) vek1 vek2
+
+let sestej vek1 vek2 =
+  let pari = List.combine vek1 vek2 in
+  List.map (fun (c1, c2) -> c1 +. c2) pari
 
 let primer_vektorji_2 = sestej [1.0; 2.0; 3.0] [4.0; 5.0; 6.0]
 (* val primer_vektorji_2 : float list = [5.; 7.; 9.] *)
@@ -35,7 +45,10 @@ let primer_vektorji_2 = sestej [1.0; 2.0; 3.0] [4.0; 5.0; 6.0]
 
 let vsota_seznama = List.fold_left (+.) 0.
 
-let skalarni_produkt _ _ = ()
+let skalarni_produkt v1 v2 = 
+  let pari = List.combine v1 v2 in
+  let nov = List.map (fun (c1, c2) -> c1 *. c2) pari in
+  vsota_seznama nov
 
 let primer_vektorji_3 = skalarni_produkt [1.0; 2.0; 3.0] [4.0; 5.0; 6.0]
 (* val primer_vektorji_3 : float = 32. *)
@@ -45,7 +58,9 @@ let primer_vektorji_3 = skalarni_produkt [1.0; 2.0; 3.0] [4.0; 5.0; 6.0]
  vektorja.
 [*----------------------------------------------------------------------------*)
 
-let norma _ = ()
+let norma vektor = 
+  let vektor2 = List.map (fun x -> x *. x) vektor in
+  sqrt(vsota_seznama vektor2)
 
 let primer_vektorji_4 = norma [3.0; 4.0]
 (* val primer_vektorji_4 : float = 5. *)
@@ -55,7 +70,8 @@ let primer_vektorji_4 = norma [3.0; 4.0]
  kot med dvema vektorjema v radianih.
 [*----------------------------------------------------------------------------*)
 
-let vmesni_kot _ _ = ()
+let vmesni_kot v1 v2 = 
+  acos(skalarni_produkt v1 v2 /. (norma v1 *. norma v2))
 
 let primer_vektorji_5 = vmesni_kot [1.0; 0.0] [0.0; 1.0]
 (* val primer_vektorji_5 : float = 1.57079632679489656 *)
@@ -65,7 +81,8 @@ let primer_vektorji_5 = vmesni_kot [1.0; 0.0] [0.0; 1.0]
  vektor.
 [*----------------------------------------------------------------------------*)
 
-let normirani _ = ()
+let normirani vek = 
+  razteg (1.0 /. (norma vek)) vek
 
 let primer_vektorji_6 = normirani [3.0; 4.0]
 (* val primer_vektorji_6 : float list = [0.600000000000000089; 0.8] *)
